68edd6b984c53a57756f519001617c8276f191a2
Ty Bekiares <tyrone.bekiares@elastic.co>
2024-03-04 16:00:00 -0600

    wip

--- livekit-plugins/livekit-plugins-azure/livekit/plugins/azure/stt.py
+++ livekit-plugins/livekit-plugins-azure/livekit/plugins/azure/stt.py
@@ -254,7 +254,10 @@ class SpeechStream(stt.SpeechStream):
             # check if audio or cntrl
             if isinstance(data, rtc.AudioFrame):
                 # resample to required rate for azure STT
-                frame = data.remix_and_resample(STT.SAMPLE_RATE, STT.NUM_CHANNELS)
+                if frame.sample_rate != STT.SAMPLE_RATE or frame.num_channels != STT.NUM_CHANNELS:
+                    frame = data.remix_and_resample(STT.SAMPLE_RATE, STT.NUM_CHANNELS)
+                else:
+                    frame = data
 
                 # if we haven't started yet, init speech recognition
                 if self._speech_recognizer == None:
